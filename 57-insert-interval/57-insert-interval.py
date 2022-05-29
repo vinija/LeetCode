014fc676b3a59aae6b7160517df5d79d7509f85e
Vinija Jain <vinija@gmail.com>
2022-05-29 00:17:04 -0700

    Time: 87 ms (82.00%), Space: 17.2 MB (92.56%) - LeetHub

---
+++ 57-insert-interval/57-insert-interval.py
@@ -0,0 +1,20 @@
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        result = []
+        
+        for interval in intervals:
+			# the new interval is after the range of other interval, so we can leave the current interval baecause the new one does not overlap with it
+            if interval[1] < newInterval[0]:
+                result.append(interval)
+            # the new interval's range is before the other, so we can add the new interval and update it to the current one
+            elif interval[0] > newInterval[1]:
+                result.append(newInterval)
+                newInterval = interval
+            # the new interval is in the range of the other interval, we have an overlap, so we must choose the min for start and max for end of interval 
+            elif interval[1] >= newInterval[0] or interval[0] <= newInterval[1]:
+                newInterval[0] = min(interval[0], newInterval[0])
+                newInterval[1] = max(newInterval[1], interval[1])
+
+        
+        result.append(newInterval); 
+        return result

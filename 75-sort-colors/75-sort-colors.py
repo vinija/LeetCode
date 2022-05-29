0f6fe1a3b8174cb6d81de2b0b59b4544829e255c
Vinija Jain <vinija@gmail.com>
2022-05-28 21:28:04 -0700

    Time: 81 ms (5.50%), Space: 13.8 MB (97.71%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.py
@@ -0,0 +1,21 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Dutch National Flag problem solution.
+        """
+        # for all idx < p0 : nums[idx < p0] = 0
+        # curr is an index of element under consideration
+        p0 = curr = 0
+        # for all idx > p2 : nums[idx > p2] = 2
+        p2 = len(nums) - 1
+
+        while curr <= p2:
+            if nums[curr] == 0:
+                nums[p0], nums[curr] = nums[curr], nums[p0]
+                p0 += 1
+                curr += 1
+            elif nums[curr] == 2:
+                nums[curr], nums[p2] = nums[p2], nums[curr]
+                p2 -= 1
+            else:
+                curr += 1

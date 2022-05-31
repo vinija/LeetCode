9823765a63ac4f77a0d9af092bbb22b6b98ba664
Vinija Jain <vinija@gmail.com>
2022-05-30 21:46:57 -0700

    Time: 123 ms (70.92%), Space: 18.3 MB (28.52%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -3,22 +3,17 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
- def mergeKLists(self, lists: List[ListNode]) -> ListNode:
-        heap = []
-        t = root = ListNode()
-        for i in range(len(lists)):
-            if lists[i]:
-                #When you put the objects (i.e. tuples) into heap, it will compare the first attribute in the object (in this case is key) to compare. If a tie happens, heap wills use the next attribute (i.e. value_1) and so on.
-                heap.append((lists[i].val, i, lists[i]))
-        heapq.heapify(heap)
-        
-        while(heap):
-            smallest = heapq.heappop(heap)
-            nxt = smallest[2].next
-            
-            root.next = ListNode(smallest[0])
-            if(nxt):
-                heapq.heappush(heap, (nxt.val,smallest[1],nxt ))
-            root = root.next
-        return t.next
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+      h = []
+      for l in lists:
+        cur = l
+        while cur:
+          heappush(h, cur.val)
+          cur = cur.next
+      dummy = cur = ListNode()        
+      while len(h) > 0:
+        cur.next = ListNode(heappop(h))
+        cur = cur.next
+      return dummy.next

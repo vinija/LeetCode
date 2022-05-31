b9ec631bdbc13df5ac3b6efda14103a4d7b0cc77
Vinija Jain <vinija@gmail.com>
2022-05-30 22:10:33 -0700

    Time: 107 ms (94.74%), Space: 15.2 MB (79.39%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.py
@@ -1,24 +1,31 @@
 class Solution:
-   def findAnagrams(self, s: str, p: str) -> List[int]:
-        needMap, output, pLen, sLen = defaultdict(int), [], len(p), len(s)
-        if pLen > sLen: return []
-
-        # build hashmap
-        for ch in p: needMap[ch] += 1
-
-        # initial full pass over the window
-        for i in range(pLen-1):
-            if s[i] in needMap: needMap[s[i]] -= 1
-            
-        # slide the window with stride 1
-        for i in range(-1, sLen-pLen+1):
-            if i > -1 and s[i] in needMap:
-                needMap[s[i]] += 1
-            if i+pLen < sLen and s[i+pLen] in needMap: 
-                needMap[s[i+pLen]] -= 1
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        indices = []
+        windowStart = 0
+        matched = 0
+        charFrequency = {}
+        
+        for char in p:
+            if char not in charFrequency:
+                charFrequency[char] = 0
+            charFrequency[char] += 1
+        
+        for windowEnd in range(len(s)):
+            rightChar = s[windowEnd]
+            if rightChar in charFrequency:
+                charFrequency[rightChar] -= 1
+                if charFrequency[rightChar] == 0:
+                    matched += 1
+                    
+            if matched == len(charFrequency):
+                indices.append(windowStart)
                 
-            # check whether we encountered an anagram
-            if all(v == 0 for v in needMap.values()): 
-                output.append(i+1)
-                
-        return output
+            if windowEnd >= len(p) - 1:
+                leftChar = s[windowStart]
+                windowStart += 1
+                if leftChar in charFrequency:
+                    if charFrequency[leftChar] == 0:
+                        matched -= 1
+                    charFrequency[leftChar] += 1
+                    
+        return indices

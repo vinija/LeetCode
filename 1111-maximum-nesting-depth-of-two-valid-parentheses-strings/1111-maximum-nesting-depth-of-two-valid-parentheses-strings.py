59dcfcaded2f1e058886a035df41ae7d1885fc87
Vinija Jain <vinija@gmail.com>
2022-05-31 22:44:11 -0700

    Time: 96 ms (8.74%), Space: 14.6 MB (51.05%) - LeetHub

--- 1111-maximum-nesting-depth-of-two-valid-parentheses-strings/1111-maximum-nesting-depth-of-two-valid-parentheses-strings.py
+++ 1111-maximum-nesting-depth-of-two-valid-parentheses-strings/1111-maximum-nesting-depth-of-two-valid-parentheses-strings.py
@@ -1,12 +1,14 @@
 class Solution:
     def maxDepthAfterSplit(self, seq: str) -> List[int]:
-        stack = [0]
-
-        for i in range(1,len(seq)):
-            if seq[i] == seq[i-1]:
-                stack.append(1-stack[i-1])
-            else:
-                stack.append(stack[i-1])
-
-        return stack
+        answer = []
+        depth = 0
         
+        for c in seq:
+            if c == '(':
+                depth += 1
+                answer.append(depth % 2)
+            else:
+                answer.append(depth % 2)
+                depth -= 1
+            
+        return answer

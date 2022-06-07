fb1f6b86ecfc3a88a5f9ae9caae18903891ede57
Vinija Jain <vinija@gmail.com>
2022-06-06 21:30:51 -0700

    Time: 59 ms (30.25%), Space: 14.9 MB (46.00%) - LeetHub

---
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.py
@@ -0,0 +1,44 @@
+class Solution(object):
+    def __init__(self):
+        # Creating a visited dictionary to hold old node reference as "key" and new node reference as the "value"
+        self.visited = {}
+
+    def getClonedNode(self, node):
+        # If node exists then
+        if node:
+            # Check if its in the visited dictionary          
+            if node in self.visited:
+                # If its in the visited dictionary then return the new node reference from the dictionary
+                return self.visited[node]
+            else:
+                # Otherwise create a new node, save the reference in the visited dictionary and return it.
+                self.visited[node] = Node(node.val, None, None)
+                return self.visited[node]
+        return None
+
+    def copyRandomList(self, head):
+        """
+        :type head: Node
+        :rtype: Node
+        """
+
+        if not head:
+            return head
+
+        old_node = head
+        # Creating the new head node.       
+        new_node = Node(old_node.val, None, None)
+        self.visited[old_node] = new_node
+
+        # Iterate on the linked list until all nodes are cloned.
+        while old_node != None:
+
+            # Get the clones of the nodes referenced by random and next pointers.
+            new_node.random = self.getClonedNode(old_node.random)
+            new_node.next = self.getClonedNode(old_node.next)
+
+            # Move one step ahead in the linked list.
+            old_node = old_node.next
+            new_node = new_node.next
+
+        return self.visited[head]

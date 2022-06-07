e7f5a2b089bed91341043c8b6a33270c11f67688
Vinija Jain <vinija@gmail.com>
2022-06-06 21:32:18 -0700

    Time: 549 ms (62.05%), Space: 27.7 MB (48.26%) - LeetHub

---
+++ 472-concatenated-words/472-concatenated-words.py
@@ -0,0 +1,29 @@
+# python code
+class Solution(object):
+    def findAllConcatenatedWordsInADict(self, words):
+        """
+        :type words: List[str]
+        :rtype: List[str]
+        """
+        d = set(words)
+        
+        def dfs(word):
+            for i in range(1, len(word)):
+                prefix = word[:i]
+                suffix = word[i:]
+                
+                if prefix in d and suffix in d:
+                    return True
+					
+                if prefix in d and dfs(suffix):
+                    return True
+                
+            
+            return False
+        
+        res = []
+        for word in words:
+            if dfs(word):
+                res.append(word)
+        
+        return res

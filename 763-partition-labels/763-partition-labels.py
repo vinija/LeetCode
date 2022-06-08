baabf5fb82b1046ced910f9b4a916f99a7074b25
Vinija Jain <vinija@gmail.com>
2022-06-07 21:03:35 -0700

    Time: 45 ms (77.48%), Space: 13.8 MB (67.50%) - LeetHub

--- 763-partition-labels/763-partition-labels.py
+++ 763-partition-labels/763-partition-labels.py
@@ -1,10 +1,14 @@
 class Solution(object):
     def partitionLabels(self, S):
+        #create a whole dictionary with key = letter value = index of last element of times that letter appears
         last = {c: i for i, c in enumerate(S)}
+        print(last)
         j = anchor = 0
         ans = []
         for i, c in enumerate(S):
+            #keep track of max frequency only
             j = max(j, last[c])
+            #make sure we met the max freq
             if i == j:
                 ans.append(i - anchor + 1)
                 anchor = i + 1

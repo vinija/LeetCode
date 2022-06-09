51768e72178c29dbe901f613f68ae5acfcdb707d
Vinija Jain <vinija@gmail.com>
2022-06-09 15:55:30 -0700

    Time: 157 ms (36.34%), Space: 15.5 MB (87.32%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,47 +1,50 @@
-from collections import defaultdict
-
-
-class Solution:
+class GNode(object):
+    """  data structure represent a vertex in the graph."""
     def __init__(self):
-        self.graph = defaultdict(list)
-        self.in_degree_list = defaultdict(int)
+        self.inDegrees = 0
+        self.outNodes = []
 
+class Solution(object):
     def canFinish(self, numCourses, prerequisites):
         """
         :type numCourses: int
         :type prerequisites: List[List[int]]
         :rtype: bool
         """
-        for prerequisite in prerequisites:
-            take_course = prerequisite[0]
-            dependent_course = prerequisite[1]
-            self.graph[dependent_course].append(take_course)
-            if dependent_course not in self.in_degree_list:
-                self.in_degree_list[dependent_course] = 0
-            self.in_degree_list[take_course] += 1
+        from collections import defaultdict, deque
+        # key: index of node; value: GNode
+        graph = defaultdict(GNode)
 
-        in_degree_zero_found = True
-        while len(self.in_degree_list) > 0:
-            in_degree_zero_found = False
-            for in_degree_node in self.in_degree_list:
-                in_degree = self.in_degree_list[in_degree_node]
-                if in_degree == 0:
-                    in_degree_zero_found = True
-                    # Remove this in_degree_node and and decrement all the adjacency's of in_degree_node by 1
-                    del self.in_degree_list[in_degree_node]
-                    adj_list = self.graph[in_degree_node]
-                    for adj_node in adj_list:
-                        if adj_node in self.in_degree_list:
-                            self.in_degree_list[adj_node] -= 1
-                    break
-            if not in_degree_zero_found:
-                return False
-        if len(self.in_degree_list) == 0:
+        totalDeps = 0
+        for relation in prerequisites:
+            nextCourse, prevCourse = relation[0], relation[1]
+            graph[prevCourse].outNodes.append(nextCourse)
+            graph[nextCourse].inDegrees += 1
+            totalDeps += 1
+
+        # we start from courses that have no prerequisites.
+        # we could use either set, stack or queue to keep track of courses with no dependence.
+        nodepCourses = deque()
+        for index, node in graph.items():
+            if node.inDegrees == 0:
+                nodepCourses.append(index)
+
+        removedEdges = 0
+        while nodepCourses:
+            # pop out course without dependency
+            course = nodepCourses.pop()
+
+            # remove its outgoing edges one by one
+            for nextCourse in graph[course].outNodes:
+                graph[nextCourse].inDegrees -= 1
+                removedEdges += 1
+                # while removing edges, we might discover new courses with prerequisites removed, i.e. new courses without prerequisites.
+                if graph[nextCourse].inDegrees == 0:
+                    nodepCourses.append(nextCourse)
+
+        if removedEdges == totalDeps:
             return True
-        return False
-
-numCourses = 4
-prerequisites = [[2,0],[1,0],[3,1],[3,2],[1,3]]
-prerequisites = [[1, 0], [2, 1], [2, 5], [0, 3], [4, 3], [3, 5], [4, 5]]
-sol = Solution()
-print(sol.canFinish(numCourses, prerequisites))
+        else:
+            # if there are still some edges left, then there exist some cycles
+            # Due to the dead-lock (dependencies), we cannot remove the cyclic edges
+            return False

6e8f4ec42da845830e7220b5ba1312af7c5c440e
Vinija Jain <vinija@gmail.com>
2022-06-11 16:54:19 -0700

    Time: 36 ms (96.19%), Space: 14.9 MB (38.57%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.py
+++ 97-interleaving-string/97-interleaving-string.py
@@ -1,23 +1,25 @@
+
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        # just interleave two linked list... let a,b,c be the head of linked list
-        @cache
-        def dp(a,b,c): # not O(N^3) but O(N^2) since 'c' is dependent -> 'a'+'b'
-            
-            # base cases
-            if c == len(s3) and b == len(s2) and a==len(s1): return True
-            elif c==len(s3): return False
-            
-            # if - TAKE THE NEXT VALUE FROM `A`
-            if a<len(s1) and \
-            s1[a] == s3[c] and dp(a+1,b,c+1): # took from `A`; move the head pointers 
+        if len(s1) + len(s2) != len(s3):
+            return False
+        
+        dp = {}
+        
+        def dfs(i, j):
+            if i == len(s1) and j == len(s2):
                 return True
             
-            # if - TAKE THE NEXT VALUE FROM `B`
-            if b< len(s2) and \
-            s2[b] == s3[c] and dp(a,b+1,c+1): # took from `B`; move the head pointers 
+            if (i, j) in dp:
+                return dp[(i, j)]
+            
+            if i < len(s1) and s1[i] == s3[i + j] and dfs(i + 1, j):
                 return True
+            if j < len(s2) and s2[j] == s3[i + j] and dfs(i, j + 1):
+                return True
+            
+            dp[(i, j)] = False
             
             return False
         
-        return dp(0,0,0)
+        return dfs(0, 0)

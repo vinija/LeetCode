2d8deba6e1cbe240300edddce2a077bdcfc28dc0
Vinija Jain <vinija@gmail.com>
2022-06-10 23:34:59 -0700

    Time: 85 ms (43.00%), Space: 14.1 MB (31.41%) - LeetHub

--- 443-string-compression/443-string-compression.py
+++ 443-string-compression/443-string-compression.py
@@ -1,17 +1,34 @@
 class Solution:
-    def compress(self, chars: List[str]) -> int:
-        if chars == None or len(chars) < 1:
-            return 0;
-        start, end, index =0, 0, 0
-        while start< len(chars):
-            end=start
-            while end< len(chars) and chars[start]==chars[end]:
-                end+=1
-            chars[index]=chars[start]
-            index+=1
-            if end-start > 1:
-                for s in str(end-start):
-                    chars[index]=s
-                    index+=1
-            start=end
-        return index
+    def compress(self, chars):
+        """
+        :type chars: List[str]
+        :rtype: int
+        """
+        # count: record the times one element had appeared. 
+        count = 1
+        # currentIdx: the idx ready to be modified.
+        currentIdx = 0
+        
+        for i in range(1, len(chars)+1): 
+            # check for valid index and if current char matches the previous char
+            # if so, add to count
+            if i < len(chars) and chars[i] == chars[i-1]:
+                count += 1
+            # When we find a new element
+            else: 
+                # 1) update letter
+                chars[currentIdx] = chars[i-1] # we modify char[currentIdx] to the .
+                currentIdx += 1
+                
+                # 2) append count to string after char
+                if count == 1:
+                    # if the previous element only appear once, skip below.
+                    continue
+                # add count digit by digit
+                for ch in str(count):
+                    chars[currentIdx] = ch
+                    currentIdx += 1
+                    
+                # 3) reset count because we're done with the current element
+                count = 1
+        return currentIdx 

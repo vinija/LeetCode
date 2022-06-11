be77f63fb445499b54e5baa143a0be2cc597b569
Vinija Jain <vinija@gmail.com>
2022-06-10 22:50:32 -0700

    Time: 71 ms (5.49%), Space: 13.9 MB (95.05%) - LeetHub

--- 399-evaluate-division/399-evaluate-division.py
+++ 399-evaluate-division/399-evaluate-division.py
@@ -1,21 +1,30 @@
 class Solution:
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        #directed graph
         graph = defaultdict(dict)
         lenght = len(equations)
         for index in range(lenght):
+            #we're building out our graph with all the connections, a->b b->a,-> c
+            #from our target, if target *2 = value
             graph[equations[index][0]][equations[index][1]] = values[index]
+            #we need inverse relationship as well, which is 1/value
             graph[equations[index][1]][equations[index][0]] = 1/values[index]
         
         def dfs(source, target, visited):
+            #these values dont even exist in our graph
             if source not in graph or target not in graph: return -1
             
+            #we found it already
             if target in graph[source]:
                 return graph[source][target]
+            
             
             for index in graph[source]:
                 if index not in visited:
                     visited.add(index)
+                    #start the search with index as the source
                     temp = dfs(index,target, visited)
+                    #we didn't find anything
                     if temp == -1:
                         continue
                     else:

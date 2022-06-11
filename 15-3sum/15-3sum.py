41bd4f91ebbb977df0512a6625878c08b6c694d3
Vinija Jain <vinija@gmail.com>
2022-06-11 14:40:02 -0700

    Time: 887 ms (73.75%), Space: 18.7 MB (11.78%) - LeetHub

---
+++ 15-3sum/15-3sum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        res, dups = set(), set()
+        seen = {}
+        for i, val1 in enumerate(nums):
+            if val1 not in dups:
+                dups.add(val1)
+                for j, val2 in enumerate(nums[i+1:]):
+                    complement = -val1 - val2
+                    if complement in seen and seen[complement] == i:
+                        res.add(tuple(sorted((val1, val2, complement))))
+                    seen[val2] = i
+        return res

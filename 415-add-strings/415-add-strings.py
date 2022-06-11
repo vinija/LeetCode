510ddb975d18b0ed1a7a7b8ea323c039278698c7
Vinija Jain <vinija@gmail.com>
2022-06-11 13:07:06 -0700

    Time: 52 ms (62.89%), Space: 13.8 MB (89.60%) - LeetHub

--- 415-add-strings/415-add-strings.py
+++ 415-add-strings/415-add-strings.py
@@ -1,7 +1,4 @@
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
-        first = int(num1)
-        second = int(num2)
-        
-        return str(first+second)
+        return f'{int(num1)+int(num2)}'
         

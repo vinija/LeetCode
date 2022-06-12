145f07f24df7d70c622252106ccac036b4ba2ee3
Vinija Jain <vinija@gmail.com>
2022-06-11 17:19:17 -0700

    Time: 73 ms (31.37%), Space: 15 MB (26.19%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.py
+++ 97-interleaving-string/97-interleaving-string.py
@@ -6,18 +6,19 @@ class Solution:
         dp = {}
         
         def dfs(indexS1, indexS2):
-            #we've reached the end of both the strings
+            #we've reached the end of both the strings base case
             if indexS1 == len(s1) and indexS2 == len(s2):
                 return True
-            
+            #we've already seen this index combination
             if (indexS1, indexS2) in dp:
                 return dp[(indexS1, indexS2)]
-            
+            #meat of the logic, valid index, and make sure the current index of s1 == indexS1 + indexs2 of s3 and run dfs with incrementing it once more
             if indexS1 < len(s1) and s1[indexS1] == s3[indexS1 + indexS2] and dfs(indexS1 + 1, indexS2):
                 return True
             if indexS2 < len(s2) and s2[indexS2] == s3[indexS1 + indexS2] and dfs(indexS1, indexS2 + 1):
                 return True
             
+            #if false, we need to record it
             dp[(indexS1, indexS2)] = False
             
             return False

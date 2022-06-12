e0c6f671ca01204fa5e5fe06ffd30d73ad4fb1ba
Vinija Jain <vinija@gmail.com>
2022-06-11 17:01:00 -0700

    Time: 52 ms (70.15%), Space: 15.1 MB (20.09%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.py
+++ 97-interleaving-string/97-interleaving-string.py
@@ -1,4 +1,3 @@
-
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
         if len(s1) + len(s2) != len(s3):
@@ -6,19 +5,20 @@ class Solution:
         
         dp = {}
         
-        def dfs(i, j):
-            if i == len(s1) and j == len(s2):
+        def dfs(indexS1, indexS2):
+            #we've reached the end of both the strings
+            if indexS1 == len(s1) and indexS2 == len(s2):
                 return True
             
-            if (i, j) in dp:
-                return dp[(i, j)]
+            if (indexS1, indexS2) in dp:
+                return dp[(indexS1, indexS2)]
             
-            if i < len(s1) and s1[i] == s3[i + j] and dfs(i + 1, j):
+            if indexS1 < len(s1) and s1[indexS1] == s3[indexS1 + indexS2] and dfs(indexS1 + 1, indexS2):
                 return True
-            if j < len(s2) and s2[j] == s3[i + j] and dfs(i, j + 1):
+            if indexS2 < len(s2) and s2[indexS2] == s3[indexS1 + indexS2] and dfs(indexS1, indexS2 + 1):
                 return True
             
-            dp[(i, j)] = False
+            dp[(indexS1, indexS2)] = False
             
             return False
         

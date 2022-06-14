51a3bddb64024f74a6ee7d79dce8de2c4268f6b7
Vinija Jain <vinija@gmail.com>
2022-06-13 22:06:00 -0700

    Time: 2824 ms (80.38%), Space: 32.2 MB (87.44%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.py
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.py
@@ -1,42 +1,11 @@
 class Solution:
     def countSmaller(self, nums: List[int]) -> List[int]:
-        # implement segment tree
-        def update(index, value, tree, size):
-            index += size  # shift the index to the leaf
-            # update from leaf to root
-            tree[index] += value
-            while index > 1:
-                index //= 2
-                tree[index] = tree[index * 2] + tree[index * 2 + 1]
-
-        def query(left, right, tree, size):
-            # return sum of [left, right)
-            result = 0
-            left += size  # shift the index to the leaf
-            right += size
-            while left < right:
-                # if left is a right node
-                # bring the value and move to parent's right node
-                if left % 2 == 1:
-                    result += tree[left]
-                    left += 1
-                # else directly move to parent
-                left //= 2
-                # if right is a right node
-                # bring the value of the left node and move to parent
-                if right % 2 == 1:
-                    right -= 1
-                    result += tree[right]
-                # else directly move to parent
-                right //= 2
-            return result
-
-        offset = 10**4  # offset negative to non-negative
-        size = 2 * 10**4 + 1  # total possible values in nums
-        tree = [0] * (2 * size)
-        result = []
-        for num in reversed(nums):
-            smaller_count = query(0, num + offset, tree, size)
-            result.append(smaller_count)
-            update(num + offset, 1, tree, size)
-        return reversed(result)
+        n = len(nums)
+        counts = [0] * n 
+        sorted_nums = sorted(nums)
+        
+        for i in range(n):
+            index = bisect.bisect_left(sorted_nums, nums[i])
+            counts[i] = index
+            del sorted_nums[index]
+        return counts

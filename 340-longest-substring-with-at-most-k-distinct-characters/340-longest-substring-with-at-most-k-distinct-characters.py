435c17eb525db4570e1c4b825f3faf6f20dc5686
Vinija Jain <vinija@gmail.com>
2022-06-14 22:49:36 -0700

    Time: 80 ms (88.64%), Space: 14.5 MB (8.97%) - LeetHub

---
+++ 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.py
@@ -0,0 +1,35 @@
+from collections import OrderedDict
+class Solution:
+    def lengthOfLongestSubstringKDistinct(self, s: str, k: int) -> int:
+        n = len(s)
+        if k == 0 or n == 0:
+            return 0
+
+        # sliding window left and right pointers
+        left, right = 0, 0
+        # hashmap character -> its rightmost position
+        # in the sliding window
+        hashmap = OrderedDict()
+
+        max_len = 1
+
+        while right < n:
+            character = s[right]
+            # if character is already in the hashmap -
+            # delete it, so that after insert it becomes
+            # the rightmost element in the hashmap
+            if character in hashmap:
+                del hashmap[character]
+            hashmap[character] = right
+            right += 1
+
+            # slidewindow contains k + 1 characters
+            if len(hashmap) == k + 1:
+                # delete the leftmost character
+                _, del_idx = hashmap.popitem(last = False)
+                # move left pointer of the slidewindow
+                left = del_idx + 1
+
+            max_len = max(max_len, right - left)
+
+        return max_len

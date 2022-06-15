75825885cfdd7246fc782fe2ea5a51eda1cc874e
Vinija Jain <vinija@gmail.com>
2022-06-14 23:00:30 -0700

    Time: 120 ms (44.02%), Space: 14.5 MB (8.97%) - LeetHub

--- 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.py
+++ 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.py
@@ -1,35 +1,19 @@
-from collections import OrderedDict
 class Solution:
     def lengthOfLongestSubstringKDistinct(self, s: str, k: int) -> int:
-        n = len(s)
-        if k == 0 or n == 0:
-            return 0
+        seen_letters = {}
+        max_len = left = 0
+        
+        for right in range(len(s)):
+            if s[right] not in seen_letters:
+                seen_letters[s[right]] = 0
+            seen_letters[s[right]] += 1
+            while len(seen_letters) > k:
+                seen_letters[s[left]] -= 1
+                if seen_letters[s[left]] == 0:
+                    del seen_letters[s[left]]
+                left += 1
 
-        # sliding window left and right pointers
-        left, right = 0, 0
-        # hashmap character -> its rightmost position
-        # in the sliding window
-        hashmap = OrderedDict()
 
-        max_len = 1
-
-        while right < n:
-            character = s[right]
-            # if character is already in the hashmap -
-            # delete it, so that after insert it becomes
-            # the rightmost element in the hashmap
-            if character in hashmap:
-                del hashmap[character]
-            hashmap[character] = right
-            right += 1
-
-            # slidewindow contains k + 1 characters
-            if len(hashmap) == k + 1:
-                # delete the leftmost character
-                _, del_idx = hashmap.popitem(last = False)
-                # move left pointer of the slidewindow
-                left = del_idx + 1
-
-            max_len = max(max_len, right - left)
-
+            max_len = max(max_len, right-left+1)
+        
         return max_len

8b024d4ddf01aa5e5b2b13d8a6e6cf5f1046cd14
Vinija Jain <vinija@gmail.com>
2022-06-16 20:54:24 -0700

    Time: 205 ms (58.74%), Space: 17.6 MB (61.60%) - LeetHub

--- 72-edit-distance/72-edit-distance.py
+++ 72-edit-distance/72-edit-distance.py
@@ -1,13 +1,34 @@
 class Solution:
-    def minDistance(self, word1: str, word2: str) -> int:
-        m, n = len(word1), len(word2)
-        dp = [n-j for j in range(n+1)]
-        for i in reversed(range(m)):
-            prev = dp[n]
-            dp[n] += 1
-            for j in reversed(range(n)): 
-                curr = dp[j]
-                if word1[i] == word2[j]: dp[j] = prev
-                else: dp[j] = 1 + min(dp[j], dp[j+1], prev)
-                prev = curr
-        return dp[0]
+    def minDistance(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: int
+        """
+        n = len(word1)
+        m = len(word2)
+        
+        # if one of the strings is empty
+        if n * m == 0:
+            return n + m
+        
+        # array to store the convertion history
+        d = [ [0] * (m + 1) for _ in range(n + 1)]
+        
+        # init boundaries
+        for i in range(n + 1):
+            d[i][0] = i
+        for j in range(m + 1):
+            d[0][j] = j
+        
+        # DP compute 
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                left = d[i - 1][j] + 1
+                down = d[i][j - 1] + 1
+                left_down = d[i - 1][j - 1] 
+                if word1[i - 1] != word2[j - 1]:
+                    left_down += 1
+                d[i][j] = min(left, down, left_down)
+        
+        return d[n][m]

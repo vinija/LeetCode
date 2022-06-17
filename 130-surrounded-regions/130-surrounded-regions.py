e98589bc85f736b831ac094e514fc00191312a07
Vinija Jain <vinija@gmail.com>
2022-06-17 12:30:33 -0700

    Time: 146 ms (89.14%), Space: 15.9 MB (52.42%) - LeetHub

---
+++ 130-surrounded-regions/130-surrounded-regions.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def solve(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: None Do not return anything, modify board in-place instead.
+        """
+        if not board or not board[0]:
+            return
+
+        self.ROWS = len(board)
+        self.COLS = len(board[0])
+
+        # Step 1). retrieve all border cells
+        from itertools import product
+        borders = list(product(range(self.ROWS), [0, self.COLS-1])) \
+                + list(product([0, self.ROWS-1], range(self.COLS)))
+
+        # Step 2). mark the "escaped" cells, with any placeholder, e.g. 'E'
+        for row, col in borders:
+            self.DFS(board, row, col)
+
+        # Step 3). flip the captured cells ('O'->'X') and the escaped one ('E'->'O')
+        for r in range(self.ROWS):
+            for c in range(self.COLS):
+                if board[r][c] == 'O':   board[r][c] = 'X'  # captured
+                elif board[r][c] == 'E': board[r][c] = 'O'  # escaped
+
+
+    def DFS(self, board, row, col):
+        if board[row][col] != 'O':
+            return
+        board[row][col] = 'E'
+        if col < self.COLS-1: self.DFS(board, row, col+1)
+        if row < self.ROWS-1: self.DFS(board, row+1, col)
+        if col > 0: self.DFS(board, row, col-1)
+        if row > 0: self.DFS(board, row-1, col)

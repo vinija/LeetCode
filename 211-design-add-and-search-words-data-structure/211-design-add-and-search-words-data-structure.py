5b148e28d76d60356b1f92b4c00256a4611dc679
Vinija Jain <vinija@gmail.com>
2022-06-17 00:36:31 -0700

    Time: 15115 ms (46.65%), Space: 56 MB (80.84%) - LeetHub

---
+++ 211-design-add-and-search-words-data-structure/211-design-add-and-search-words-data-structure.py
@@ -0,0 +1,44 @@
+class WordDictionary:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.trie = {}
+
+
+    def addWord(self, word: str) -> None:
+        """
+        Adds a word into the data structure.
+        """
+        node = self.trie
+
+        for ch in word:
+            if not ch in node:
+                node[ch] = {}
+            node = node[ch]
+        node['$'] = True
+
+    def search(self, word: str) -> bool:
+        """
+        Returns if the word is in the data structure. A word could contain the dot character '.' to represent any letter.
+        """
+        def search_in_node(word, node) -> bool:
+            for i, ch in enumerate(word):
+                if not ch in node:
+                    # if the current character is '.'
+                    # check all possible nodes at this level
+                    if ch == '.':
+                        for x in node:
+                            if x != '$' and search_in_node(word[i + 1:], node[x]):
+                                return True
+                    # if no nodes lead to answer
+                    # or the current character != '.'
+                    return False
+                # if the character is found
+                # go down to the next level in trie
+                else:
+                    node = node[ch]
+            return '$' in node
+
+        return search_in_node(word, self.trie)

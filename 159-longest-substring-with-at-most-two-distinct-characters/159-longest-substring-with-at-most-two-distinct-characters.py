e94c1eb90be89e9eb8682ea480bc34e8eae3eff8
Vinija Jain <vinija@gmail.com>
2022-06-19 23:04:35 -0700

    Time: 978 ms (8.54%), Space: 14.7 MB (86.81%) - LeetHub

--- 159-longest-substring-with-at-most-two-distinct-characters/159-longest-substring-with-at-most-two-distinct-characters.py
+++ 159-longest-substring-with-at-most-two-distinct-characters/159-longest-substring-with-at-most-two-distinct-characters.py
@@ -1,29 +1,22 @@
 class Solution:
     def lengthOfLongestSubstringTwoDistinct(self, s: str) -> int:
-        seen = Counter()
         start = 0
+        seen = Counter()
         maxCount = 0
         
-        # sliding window from start to end
         for end, char in enumerate(s):
-            # saw a char? add it to our hashmap
-            seen[char] += 1
-            
-            # do you have more chars than what we need?
-            while len(seen) > 2:
-                # start removing character from left
+            seen[char] +=1
+            while len(seen)>2:
                 leftChar = s[start]
-                seen[leftChar] -= 1
+                seen[leftChar] -=1
                 
-                # if count for a char is 0, delete it from our hashmap
                 if seen[leftChar] == 0:
                     del seen[leftChar]
-                    
-                # move the left pointer ahead
-                start += 1
                 
-            # current window
-            count = end - start + 1
-            maxCount = max(count, maxCount)
+                start +=1
+            
+            maxCount = max(maxCount, end - start +1)
         
         return maxCount
+
+    

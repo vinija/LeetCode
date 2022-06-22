397fcbbaa12657e74b2ed0a0fb72db6887c68358
Vinija Jain <vinija@gmail.com>
2022-06-21 22:05:56 -0700

    Time: 136 ms (25.29%), Space: 14.8 MB (71.94%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.py
@@ -1,19 +1,5 @@
-from queue import PriorityQueue 
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        pq = PriorityQueue()
-        for num in nums:
-            pq.put(num)
-            
-        numNeed = len(nums) - k
-        
-        while pq and numNeed > 0:
-            print(pq.get())
-            numNeed-=1
-            print(k)
-            
-        
-        return pq.get()
-        
-        
+
+        return heapq.nlargest(k,nums)[-1]
         

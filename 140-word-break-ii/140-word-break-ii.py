b5202ea3e0c876f34116f67905ee9edc19a111ce
Vinija Jain <vinija@gmail.com>
2022-06-22 20:34:27 -0700

    Time: 33 ms (87.80%), Space: 14 MB (32.99%) - LeetHub

--- 140-word-break-ii/140-word-break-ii.py
+++ 140-word-break-ii/140-word-break-ii.py
@@ -1,18 +1,24 @@
-
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
-        ans = []
-        wordDict = set(wordDict)
-        def dfs(subpath, string):
-            if string == "":
-                ans.append(" ".join(subpath))
-                return
-            
-            for word in wordDict:
-                if string.startswith(word):
-                    subpath.append(word)
-                    dfs(subpath, string[len(word):])
-                    subpath.pop()
-            
-        dfs([], s)
-        return ans
+        # quick check on the characters,
+        #   otherwise it would exceed the time limit for certain test cases.
+        if set(Counter(s).keys()) > set(Counter("".join(wordDict)).keys()):
+            return []
+
+        wordSet = set(wordDict)
+
+        dp = [[]] * (len(s)+1)
+        dp[0] = [""]
+
+        for endIndex in range(1, len(s)+1):
+            sublist = []
+            # fill up the values in the dp array.
+            for startIndex in range(0, endIndex):
+                word = s[startIndex:endIndex]
+                if word in wordSet:
+                    for subsentence in dp[startIndex]:
+                        sublist.append((subsentence + ' ' + word).strip())
+
+            dp[endIndex] = sublist
+
+        return dp[len(s)]

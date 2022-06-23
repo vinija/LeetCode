b17288bbbe51e195ec52c5410d52d05b13afb31c
Vinija Jain <vinija@gmail.com>
2022-06-22 21:35:17 -0700

    Time: 1362 ms (25.68%), Space: 20.3 MB (59.26%) - LeetHub

---
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.py
@@ -0,0 +1,11 @@
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        # Sort the list with a custom comparator function
+        points.sort(key=self.squared_distance)
+        
+        # Return the first k elements of the sorted list
+        return points[:k]
+    
+    def squared_distance(self, point: List[int]) -> int:
+        """Calculate and return the squared Euclidean distance."""
+        return point[0] ** 2 + point[1] ** 2

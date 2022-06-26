ad2bfa474ba0c8de326b8703294ac859df42079a
Vinija Jain <vinija@gmail.com>
2022-06-25 23:30:42 -0700

    Time: 79 ms (82.03%), Space: 13.9 MB (46.80%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.py
@@ -4,4 +4,6 @@ class Solution:
         for i, n in enumerate(sorted(nums)):
             if n not in dct:
                 dct[n] = i
+        print(dct)
+        #{1: 0, 2: 1, 3: 3, 8: 4}
         return [dct[n] for n in nums]

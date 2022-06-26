08d18c9a1a7cf0bc69f17dae2f26af59b3a86211
Vinija Jain <vinija@gmail.com>
2022-06-25 18:57:22 -0700

    Time: 897 ms (50.51%), Space: 28.4 MB (33.87%) - LeetHub

--- 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.py
+++ 1567-maximum-length-of-subarray-with-positive-product/1567-maximum-length-of-subarray-with-positive-product.py
@@ -1,3 +1,16 @@
+'''As we traverse through nums, we keep a running size/length of positive and negative products, represented by pos and neg, respectively
+
+The logic is as follows:
+if current element is positive then
+-positive will be the previous positive length + 1 (because positive x positive = positive)
+-negative will be the previous negative length + 1 (because positive x negative = negative and only if a negative product length exists, otherwise its zero)
+if current element is negative then
+-positive will be the previous negative length + 1 (because negative x negative = positive but only if a neg exists, otherwise its zero)
+-negative will be the previous positive length + 1 (because positive x negative = negative)
+if current element is zero then
+-reset all lengths to zero
+
+The questions is concerned with the maximum length subarray of positive products, so we only use this to update ans.'''
 class Solution:
     def getMaxLen(self, nums: List[int]) -> int:
             n = len(nums)

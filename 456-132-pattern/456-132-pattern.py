02daff386b0984826fc186e816bf63c21b7a1cfc
Vinija Jain <vinija@gmail.com>
2022-06-26 18:36:47 -0700

    Time: 486 ms (45.65%), Space: 32.3 MB (12.79%) - LeetHub

--- 456-132-pattern/456-132-pattern.py
+++ 456-132-pattern/456-132-pattern.py
@@ -1,17 +1,12 @@
 class Solution:
     def find132pattern(self, nums: List[int]) -> bool:
-        if len(nums)<3:
-            return False
-        
-        second_num = -math.inf
-        stck = []
-        # Try to find nums[i] < second_num < stck[-1]
-        for i in range(len(nums) - 1, -1, -1):
-            if nums[i] < second_num:
+        stack = []
+        num = float('-inf')
+        for n in nums[::-1]:
+            
+            while stack and stack[-1] < n:
+                num = stack.pop()
+            if n < num:
                 return True
-            # always ensure stack can be popped in increasing order
-            while stck and stck[-1] < nums[i]:
-                second_num = stck.pop()  # this will ensure  second_num < stck[-1] for next iteration
-
-            stck.append(nums[i])
+            stack.append(n)
         return False

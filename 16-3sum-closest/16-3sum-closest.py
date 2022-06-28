559d18b0e576e0aaad225eb5a6879580a5c5ec72
Vinija Jain <vinija@gmail.com>
2022-06-28 14:25:38 -0700

    Time: 9806 ms (5.01%), Space: 14.4 MB (6.17%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.py
+++ 16-3sum-closest/16-3sum-closest.py
@@ -1,44 +1,25 @@
 class Solution:
-    
+
     def threeSumClosest(self, nums: List[int], target: int) -> int:
+        min_diff = math.inf
+        r = None 
         nums.sort()
-        res = float('inf')
-        for i in range(len(nums)):
-            l, r = i + 1, len(nums) - 1
-            while l < r:
-                s = nums[i] + nums[l] + nums[r]
-                if abs(s - target) < abs(res - target):
-                    res = s
-                if s == target:
-                    return target
-                elif s < target:
-                    l += 1
+        n = len(nums)
+        print(nums)
+        
+        for i in range(n):
+            lo = i + 1
+            hi = n - 1
+            while lo < hi:
+                csum = nums[i] + nums[lo] + nums[hi]
+                diff = abs(csum - target)
+                if diff < min_diff:
+                    min_diff = diff
+                    r = csum
+                if csum < target:
+                    lo += 1
+                elif csum > target:
+                    hi -= 1
                 else:
-                    r -= 1
-        return res
-#     def threeSumClosest(self, nums: List[int], target: int) -> int:
-#         diff = float('inf')
-#         answer = 0
-#         length = len(nums)
-        
-#         nums.sort()
-        
-#         for index in range(length):
-#             low = index + 1
-#             high = length -1
-            
-#             while low < high:
-#                 currSum = nums[index] + nums[low] + nums[high]
-#                 localDiff = abs(target-currSum)
-                
-#                 if localDiff == 0:
-#                     return currSum
-#                 if localDiff < diff:
-#                     diff = localDiff
-#                     answer = currSum
-#                 if currSum < target:
-#                     low +=1
-#                 else:
-#                     high +=1
-        
-#         return answer
+                    return r
+        return r 

a2a04327bd39a45b11f1a7d9fd65429961750ab0
Vinija Jain <vinija@gmail.com>
2022-06-28 23:10:36 -0700

    Time: 392 ms (61.44%), Space: 15 MB (21.98%) - LeetHub

--- 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.py
+++ 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.py
@@ -1,11 +1,14 @@
+
 class Solution:
     def checkXMatrix(self, grid: List[List[int]]) -> bool:
-            m, n = len(grid), len(grid[0]) # problem states that m == n
-
-            def isDiagonal(i,j):
-                return i == j or (i + j + 1 == m)
-
-            def isValid(i,j):
-                return isDiagonal(i,j) == bool(grid[i][j]) # if it's diagonal, it's != 0; if it's not diagonal, it's 0
-
-            return all(isValid(i,j) for i,j in product(range(m), range(n)))
+        row = len(grid)
+        col = len(grid[0])
+        for r in range(row):
+            for c in range(col):
+                if (r == c) or (r+c == row-1):
+                    if grid[r][c] == 0:
+                        return False
+                if (r != c ) and (r+c != row-1 ):
+                    if grid[r][c] != 0:
+                        return False
+        return True

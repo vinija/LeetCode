ae6052f354c6037c4638f7286d48996d6048103b
Vinija Jain <vinija@gmail.com>
2022-06-28 23:32:47 -0700

    Time: 66 ms (89.41%), Space: 16.3 MB (13.96%) - LeetHub

--- 260-single-number-iii/260-single-number-iii.py
+++ 260-single-number-iii/260-single-number-iii.py
@@ -1,12 +1,23 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
-        counter = Counter(nums)
-        n= 2
-        #reverse most common
-        output = counter.most_common()[:-n-1:-1]
-        ret = []
-        for item, freq in output:
-            ret.append(item)
+
+        seen = set()
+
+        for num in nums:
+            if num in seen:
+                seen.remove(num)
+            else:
+                seen.add(num)
+
+        return list(seen)
+#     def singleNumber(self, nums: List[int]) -> List[int]:
+#         counter = Counter(nums)
+#         n= 2
+#         #reverse most common
+#         output = counter.most_common()[:-n-1:-1]
+#         ret = []
+#         for item, freq in output:
+#             ret.append(item)
         
-        return ret
+#         return ret
         

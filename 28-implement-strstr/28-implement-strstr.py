53152b334cf56f60af66eb28038585813a8e39c2
Vinija Jain <vinija@gmail.com>
2022-06-30 21:38:05 -0700

    Time: 40 ms (68.32%), Space: 13.9 MB (63.42%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.py
+++ 28-implement-strstr/28-implement-strstr.py
@@ -1,3 +1,17 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
-        return haystack.find(needle)
+        #return haystack.find(needle)
+            
+        #needle = "c"
+        #haystack = "abc"
+        
+        if len(needle) == 0 :return 0
+        
+        lenofNeedle = len(needle)
+        
+        for i in range(len(haystack) - lenofNeedle + 1):
+            print(f'word is {haystack[i:lenofNeedle]} , index {i}')
+            if haystack[i:lenofNeedle + i] == needle :
+                return i
+            
+        return - 1

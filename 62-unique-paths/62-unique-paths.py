1c792d4cfdd780396975dcf79d10c2cdebb9ea29
Vinija Jain <vinija@gmail.com>
2022-06-30 23:08:43 -0700

    Time: 47 ms (52.03%), Space: 13.8 MB (73.43%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -2,7 +2,7 @@ class Solution:
 
     def uniquePaths(self, m: int, n: int) -> int:
         d = [[1] * n for _ in range(m)]
-        #trick question as it will take the same time going down or going left
+        #trick question as it will take the same time going down or going left first
         for col in range(1, m):
             for row in range(1, n):
                 d[col][row] = d[col - 1][row] + d[col][row - 1]

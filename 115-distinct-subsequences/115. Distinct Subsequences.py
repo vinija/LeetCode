1ffb494629e743456c3269a401fad611b5111c1e
Vinija Jain <vinija@gmail.com>
2024-07-15 13:46:44 -0700

    Runtime: 291 ms (62.56%), Memory: 73 MB (48.21%) - Gitcode

---
+++ 115-distinct-subsequences/115. Distinct Subsequences.py
@@ -0,0 +1,19 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        m, n = len(s), len(t)
+
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+        for i in range(m + 1):
+            dp[i][0] = 1
+        
+        #fill out the DP table
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if s[i - 1] == t[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j]
+                else:
+                    dp[i][j] = dp[i - 1][j]
+        
+        return dp[m][n]
+        

70d6f2d0d5d4868654ac3df14f6bcc9d9a84938d
Vinija Jain <vinija@gmail.com>
2024-08-05 13:02:19 -0700

    Runtime: 39 ms (25.55%), Memory: 16.6 MB (10.04%) - Gitcode

---
+++ 140-word-break-ii/140. Word Break II.py
@@ -0,0 +1,25 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        # quick check on the characters,
+        #   otherwise it would exceed the time limit for certain test cases.
+        if set(Counter(s).keys()) > set(Counter("".join(wordDict)).keys()):
+            return []
+
+        wordSet = set(wordDict)
+
+        dp = [[]] * (len(s)+1)
+        dp[0] = [""]
+
+        for endIndex in range(1, len(s)+1):
+            sublist = []
+            # fill up the values in the dp array.
+            for startIndex in range(0, endIndex):
+                word = s[startIndex:endIndex]
+                if word in wordSet:
+                    for subsentence in dp[startIndex]:
+                        sublist.append((subsentence + ' ' + word).strip())
+                        print(sublist)
+            
+            dp[endIndex] = sublist
+
+        return dp[len(s)]

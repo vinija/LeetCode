1e29a7087dca8b97a7d4acdc2a4e2f4ee5453f13
Vinija Jain <vinija@gmail.com>
2024-08-12 11:28:16 -0700

    Runtime: 123 ms (13.90%), Memory: 18.5 MB (21.59%) - Gitcode

--- 42-trapping-rain-water/42. Trapping Rain Water.py
+++ 42-trapping-rain-water/42. Trapping Rain Water.py
@@ -1,26 +1,22 @@
+from typing import List
+
 class Solution:
     def trap(self, height: List[int]) -> int:
+        if not height:
+            return 0
         
-        if not height: return 0
-        #two pointer approach
-        left = 0
-        right = len(height) - 1
-        answer = 0
-        
-        #when water is greater at one end, left or right, we only need to subtract it from that direction
-        leftMax = height[left]
-        rightMax = height[right]
+        left, right = 0, len(height) - 1
+        left_max, right_max = height[left], height[right]
+        water_trapped = 0
         
         while left < right:
-            #calculate left side
-            if leftMax < rightMax:
-                left+=1
-                leftMax = max(leftMax,height[left] )
-                answer += leftMax - height[left]
-            #add in right side so its all in one pass
+            if left_max < right_max:
+                left += 1
+                left_max = max(left_max, height[left])
+                water_trapped += left_max - height[left]
             else:
-                right -=1
-                rightMax = max(rightMax, height[right])
-                answer += rightMax - height[right]
+                right -= 1
+                right_max = max(right_max, height[right])
+                water_trapped += right_max - height[right]
         
-        return answer
+        return water_trapped

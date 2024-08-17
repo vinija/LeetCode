ba9383d656844083182b49ab6c49780fc04033aa
Vinija Jain <vinija@gmail.com>
2024-08-17 15:27:35 -0700

    Runtime: 35 ms (72.08%), Memory: 16.5 MB (81.32%) - Gitcode

--- 289-game-of-life/289. Game of Life.py
+++ 289-game-of-life/289. Game of Life.py
@@ -3,44 +3,28 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
+        rows = len(board)
+        cols = len(board[0])
+        directions = [[0, 1], [1, 0], [0, -1], [-1, 0], [1, 1], [1, -1], [-1, 1], [-1, -1]]
         
-        #get dimensions of the board
-        rowLen = len(board[0])
-        colLen = len(board)
+        liveCount = 0
+        toChange = {}
 
-        # Create a copy of the board to store the updated states
-        ans = [[-1 for x in range(rowLen)] for y in range(colLen)]
+        def valid(r, c):
+            return (0 <= r < rows) and (0 <= c < cols)
 
-        # Helper function to check if a cell is within the board's bounds
-        def inBound(i, j):
-            return (0 <= i < colLen) and (0 <= j < rowLen)
-        
-        for i in range(colLen):
-            for j in range(rowLen):
-                count = 0
-
-                # Check the eight possible neighbors
-                for ii, jj in [
-                    (i, j+1), (i, j-1), (i-1, j), (i+1, j), 
-                    (i-1, j+1), (i+1, j-1), (i+1, j+1), (i-1, j-1)
-                ]:
-                    if inBound(ii, jj):  # Ensure the neighbor is within bounds
-                        count += board[ii][jj]  # Count live neighbors
-                                # Apply the Game of Life rules to determine the next state of the cell
-                if count < 2:
-                    ans[i][j] = 0  # Rule 1: Underpopulation
-                elif board[i][j] == 1 and (count == 2 or count == 3):
-                    ans[i][j] = 1  # Rule 2: Survival
-                elif board[i][j] == 1 and count > 3:
-                    ans[i][j] = 0  # Rule 3: Overpopulation
-                elif board[i][j] == 0 and count == 3:
-                    ans[i][j] = 1  # Rule 4: Reproduction
+        for r in range(rows):
+            for c in range(cols):
+                liveCount = 0
+                for dr, dc in directions:
+                    newRow, newCol = r + dr, c + dc
+                    if valid(newRow, newCol) and board[newRow][newCol] == 1:
+                        liveCount += 1
+                if board[r][c] == 1:
+                    if liveCount < 2 or liveCount > 3:
+                        toChange[(r, c)] = 0 
                 else:
-                    ans[i][j] = board[i][j]  # Keep the state if no rule applies
-        board[:] = ans
-
-        return board
-                
-
-
-
+                    if liveCount == 3:
+                        toChange[(r, c)] = 1  
+        for (r, c), nextState in toChange.items():
+            board[r][c] = nextState

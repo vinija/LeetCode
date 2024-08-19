bccfc6aa27d52bdfd48af65979eb8d5346d007dc
Vinija Jain <vinija@gmail.com>
2024-08-19 15:23:18 -0700

    Runtime: 32 ms (74.19%), Memory: 16.4 MB (73.66%) - Gitcode

--- 458-poor-pigs/458. Poor Pigs.py
+++ 458-poor-pigs/458. Poor Pigs.py
@@ -1,4 +1,12 @@
+from math import log, ceil, isclose
+
 class Solution:
     def poorPigs(self, buckets: int, minutes_to_die: int, minutes_to_test: int) -> int:
-        pigs = log(buckets, minutes_to_test // minutes_to_die + 1)
+        # Calculate the number of tests possible
+        tests = minutes_to_test // minutes_to_die
+        
+        # Calculate the number of pigs required using logarithms
+        pigs = log(buckets) / log(tests + 1)
+        
+        # Return the smallest integer greater than or equal to pigs
         return round(pigs) if isclose(pigs, round(pigs)) else ceil(pigs)

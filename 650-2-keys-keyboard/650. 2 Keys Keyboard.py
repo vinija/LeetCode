70060e176cb23da67af1f92d0b09f9a580fba395
Vinija Jain <vinija@gmail.com>
2024-08-18 20:50:26 -0700

    Runtime: 35 ms (86.22%), Memory: 16.6 MB (61.96%) - Gitcode

--- 650-2-keys-keyboard/650. 2 Keys Keyboard.py
+++ 650-2-keys-keyboard/650. 2 Keys Keyboard.py
@@ -1,12 +1,21 @@
 class Solution:
     def minSteps(self, n: int) -> int:
+        # Initialize the total steps required to 0
         steps = 0
+        
+        # Start with the smallest possible factor, which is 2
         factor = 2
-
+        
+        # Factorize the number n
         while n > 1:
+            # While n is divisible by the current factor
             while n % factor == 0:
+                # Add the factor to the steps (since we need this many "Paste" operations)
                 steps += factor
+                # Divide n by this factor to reduce it
                 n //= factor
+            # Move to the next possible factor
             factor += 1
-
+        
+        # Return the total number of steps required
         return steps

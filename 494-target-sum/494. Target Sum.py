f77d8be2ed04be80e1bf4a2f216d7393beb43781
Vinija Jain <vinija@gmail.com>
2024-08-19 17:18:48 -0700

    Runtime: 152 ms (77.57%), Memory: 16.6 MB (94.91%) - Gitcode

--- 494-target-sum/494. Target Sum.py
+++ 494-target-sum/494. Target Sum.py
@@ -1,23 +1,18 @@
+from typing import List
+from collections import defaultdict
+
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        total_sum = sum(nums)
-
-        #If target is out of the possible range, return 0
-        if total_sum < abs(target) or (total_sum + target) %2 != 0:
-            return 0
-        
-        #calculate the required subset sum
-        # (P+N) + (P-N) = total_sum + target
-        # P = total_sum + target // 2
-        subset_sum = (total_sum + target) // 2
-
-        dp = [0] * (subset_sum + 1)
-        dp[0] = 1
+        dp = defaultdict(int)
+        dp[0] = 1# base case
 
         for num in nums:
-            for j in range(subset_sum, num - 1, -1):
-                dp[j] += dp[j-num]
+            next_dp = defaultdict(int)
+            for current_sum in dp:
+                next_dp[current_sum + num] += dp[current_sum]
+                next_dp[current_sum - num] += dp[current_sum]
+            dp = next_dp
         
-        return dp[subset_sum]
+        return dp[target]
 
-
+        

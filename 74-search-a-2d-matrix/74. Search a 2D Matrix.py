01683c4281157fecd78029f3a34e7b7adb24a762
Vinija Jain <vinija@gmail.com>
2024-08-20 16:38:30 -0700

    Runtime: 44 ms (68.09%), Memory: 17.1 MB (60.29%) - Gitcode

---
+++ 74-search-a-2d-matrix/74. Search a 2D Matrix.py
@@ -0,0 +1,23 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix or not matrix[0]:
+            return False
+        
+        m, n = len(matrix), len(matrix[0])
+        left, right = 0, m * n - 1
+        
+        while left <= right:
+            mid = left + (right - left) // 2
+            # Convert the mid index to the corresponding row and column
+            row = mid // n
+            col = mid % n
+            mid_value = matrix[row][col]
+            
+            if mid_value == target:
+                return True
+            elif mid_value < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return False

07f586c2001ec6146c69cbed48b367fa6884d666
Vinija Jain <vinija@gmail.com>
2024-08-19 17:52:09 -0700

    Runtime: 31 ms (79.70%), Memory: 16.5 MB (62.75%) - Gitcode

---
+++ 225-implement-stack-using-queues/225. Implement Stack using Queues.py
@@ -0,0 +1,21 @@
+from collections import deque
+
+class MyStack:
+
+    def __init__(self):
+        self.queue = deque()
+
+    def push(self, x: int) -> None:
+        self.queue.append(x)
+        # Rotate the queue to make the last added element the front of the queue
+        for _ in range(len(self.queue) - 1):
+            self.queue.append(self.queue.popleft())
+
+    def pop(self) -> int:
+        return self.queue.popleft()
+
+    def top(self) -> int:
+        return self.queue[0]
+
+    def empty(self) -> bool:
+        return len(self.queue) == 0

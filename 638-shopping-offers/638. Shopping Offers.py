9e4d8709fc67835e05ad8186993391c82e861f86
Vinija Jain <vinija@gmail.com>
2024-08-21 16:09:16 -0700

    Runtime: 45 ms (100.00%), Memory: 16.8 MB (36.28%) - Gitcode

--- 638-shopping-offers/638. Shopping Offers.py
+++ 638-shopping-offers/638. Shopping Offers.py
@@ -1,27 +1,32 @@
 class Solution:
     def shoppingOffers(self, price: List[int], special: List[List[int]], needs: List[int]) -> int:
-        
+        # Memoization dictionary to store the results for different needs
         memo = {}
-
-        def dfs(currentNeeds):
-
-            needsTuple = tuple(currentNeeds)
-            if needsTuple in memo:
-                return memo[needsTuple]
+        
+        def dfs(current_needs):
+            # Convert current needs to a tuple to use as a key in memoization dictionary
+            needs_tuple = tuple(current_needs)
             
-            minCost = sum(currentNeeds[i] * price[i] for i in range(len(price)))
-
+            # If already computed, return the stored result
+            if needs_tuple in memo:
+                return memo[needs_tuple]
+            
+            # Calculate the cost without any special offers
+            total_cost = sum(current_needs[i] * price[i] for i in range(len(needs)))
+            
+            # Try to apply each special offer
             for offer in special:
-                newNeeds = currentNeeds [:]
-
-                for i in range(len(price)):
-                    if offer[i] > newNeeds[i]:
+                new_needs = []
+                for i in range(len(current_needs)):
+                    if current_needs[i] < offer[i]:  # Cannot use this offer
                         break
-                else:
-                    for i in range(len(price)):
-                        newNeeds[i] -= offer[i]
-                    offerCost = offer[-1] + dfs(newNeeds)
-                    minCost = min(minCost, offerCost)
-            memo[needsTuple] = minCost
-            return minCost
+                    new_needs.append(current_needs[i] - offer[i])
+                else:  # If we didn't break, the offer is valid
+                    total_cost = min(total_cost, offer[-1] + dfs(new_needs))
+            
+            # Store the result in memo dictionary
+            memo[needs_tuple] = total_cost
+            return total_cost
+        
+        # Start the recursive process
         return dfs(needs)

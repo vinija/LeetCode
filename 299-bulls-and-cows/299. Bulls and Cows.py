3f505e6b841444bc84804fb1853b0f136ed7f2e1
Vinija Jain <vinija@gmail.com>
2024-08-21 14:10:00 -0700

    Runtime: 42 ms (61.30%), Memory: 16.6 MB (53.79%) - Gitcode

--- 299-bulls-and-cows/299. Bulls and Cows.py
+++ 299-bulls-and-cows/299. Bulls and Cows.py
@@ -2,18 +2,19 @@ class Solution:
     def getHint(self, secret: str, guess: str) -> str:
         bulls = 0
         cows = 0
-
-        secret_counter = Counter()
-        guess_counter = Counter()
-
+        secret_count = [0] * 10
+        guess_count = [0] * 10
+        
+        # First pass to count bulls
         for i in range(len(secret)):
             if secret[i] == guess[i]:
                 bulls += 1
             else:
-                secret_counter[secret[i]] += 1
-                guess_counter[guess[i]] += 1
+                secret_count[int(secret[i])] += 1
+                guess_count[int(guess[i])] += 1
         
-        for digit in guess_counter:
-            if digit in secret_counter:
-                cows += min(secret_counter[digit], guess_counter[digit])
+        # Second pass to count cows
+        for i in range(10):
+            cows += min(secret_count[i], guess_count[i])
+        
         return f"{bulls}A{cows}B"

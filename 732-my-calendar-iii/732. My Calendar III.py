6015a673a151208f2271b19093005db120978e3c
Vinija Jain <vinija@gmail.com>
2024-08-21 16:45:01 -0700

    Runtime: 1097 ms (14.58%), Memory: 16.8 MB (95.74%) - Gitcode

--- 732-my-calendar-iii/732. My Calendar III.py
+++ 732-my-calendar-iii/732. My Calendar III.py
@@ -1,27 +1,28 @@
-from collections import defaultdict
-
 class MyCalendarThree:
 
     def __init__(self):
-        self.delta = defaultdict(int)
-        
+        # Initialize a dictionary to keep track of all events
+        # The key is the time (start or end), and the value is the number of events starting or ending at that time
+        self.events = {}
 
     def book(self, startTime: int, endTime: int) -> int:
-
-        self.delta[startTime] += 1
-        self.delta[endTime] -= 1
-
-        ongoing_events = 0
+        # Increase the count of events starting at `startTime`
+        if startTime in self.events:
+            self.events[startTime] += 1
+        else:
+            self.events[startTime] = 1
+        
+        # Decrease the count of events ending at `endTime`
+        if endTime in self.events:
+            self.events[endTime] -= 1
+        else:
+            self.events[endTime] = -1
+        
+        # Traverse through the events to find the maximum number of overlapping events
+        ongoing = 0
         max_overlap = 0
-
-        for time in sorted(self.delta):
-            ongoing_events += self.delta[time]
-            max_overlap = max(max_overlap, ongoing_events)
+        for time in sorted(self.events):
+            ongoing += self.events[time]
+            max_overlap = max(max_overlap, ongoing)
         
         return max_overlap
-        
-
-
-# Your MyCalendarThree object will be instantiated and called as such:
-# obj = MyCalendarThree()
-# param_1 = obj.book(startTime,endTime)

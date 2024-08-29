a1a68250a4b308b824dc7ba1efd2b94066492b26
Vinija Jain <vinija@gmail.com>
2024-08-28 17:07:46 -0700

    Runtime: 105 ms (61.36%), Memory: 17.7 MB (42.38%) - Gitcode

---
+++ 45-jump-game-ii/45. Jump Game II.py
@@ -0,0 +1,24 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        
+        if len(nums) == 1:
+            return 0
+        
+        jumps = 0
+        farthest = 0
+        current_end = 0
+
+        for i in range(len(nums) -1):
+            #update the farthest position we can reach
+            farthest = max(farthest, i + nums[i])
+
+            #If we reach the end of the current jump range
+            if i == current_end:
+                jumps += 1 #make a jump
+                current_end = farthest #update the end to farthest
+
+                #If the farthest point reaches or exceeds the last index, we can break
+                if current_end >= len(nums) - 1:
+                    break
+        
+        return jumps

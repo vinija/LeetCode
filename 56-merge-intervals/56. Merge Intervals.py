5b72e651dfe62b29d243f7db3a3edc20811b8870
Vinija Jain <vinija@gmail.com>
2024-09-14 16:29:51 -0700

    Runtime: 118 ms (82.30%), Memory: 21.2 MB (13.81%) - Gitcode

--- 56-merge-intervals/56. Merge Intervals.py
+++ 56-merge-intervals/56. Merge Intervals.py
@@ -3,20 +3,26 @@ from typing import List
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
 
-        #Sort intervals by the start time
-        intervals.sort(key=lambda x: x[0])
+        if not intervals or len(intervals) == 1:
+            return intervals
+        
+        sorted_intervals = sorted(intervals, key=lambda x: x[0])
 
-        #initialize the results list
         merged = []
 
-        for interval in intervals:
-            #append if merged list is empty or no overlap
-            if not merged or merged[-1][1] < interval[0]:
-                merged.append(interval)
-            else:
-                #overlap exists, merge
-                merged[-1][1] = max(merged[-1][1], interval[1])
-            
+        for index, current in enumerate(sorted_intervals):
+            current_start, current_end = current
 
+            if not merged:
+                merged.append([current_start, current_end])
+                continue
+            
+            last_merged_start, last_merged_end = merged[-1]
+
+            if current_start <= last_merged_end:
+                merged[-1][1] = max(last_merged_end, current_end)
+            else:
+                merged.append([current_start, current_end])
         return merged
+        
         

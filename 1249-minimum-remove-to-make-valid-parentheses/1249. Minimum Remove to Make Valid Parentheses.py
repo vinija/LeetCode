31e3674733d6973be2e55de64e9306fe726636bb
Vinija Jain <vinija@gmail.com>
2024-09-13 21:09:40 -0700

    Runtime: 87 ms (41.49%), Memory: 18.8 MB (29.98%) - Gitcode

--- 1249-minimum-remove-to-make-valid-parentheses/1249. Minimum Remove to Make Valid Parentheses.py
+++ 1249-minimum-remove-to-make-valid-parentheses/1249. Minimum Remove to Make Valid Parentheses.py
@@ -1,14 +1,10 @@
-from typing import List, Set, Optional
-
 class Solution:
     def minRemoveToMakeValid(self, s: str) -> str:
-        #stack to keep track of indices of (
+        # Keep track of the indices of invalid parens
+        invalid_indicies = set()
         stack = []
 
-        #set to keep track of indices to remove
-        to_remove = set()
-
-        #first pass to find indices of invalid parens
+        #Identify invalid parens
         for i, char in enumerate(s):
             if char == '(':
                 stack.append(i)
@@ -16,18 +12,10 @@ class Solution:
                 if stack:
                     stack.pop()
                 else:
-                    to_remove.add(i)
-        
-        #if anything left in stack, add to be removed
-        to_remove = to_remove.union(set(stack))
+                    invalid_indicies.add(i)
 
-        #second pass to create the valid string
-        result = []
-        
-        for i, char in enumerate(s):
-            if i not in to_remove:
-                result.append(char)
-        
-        return ''.join(result)
+        #add to set
+        invalid_indicies.update(stack)
 
-        
+        #return
+        return "".join([char for i, char in enumerate(s) if i not in invalid_indicies])

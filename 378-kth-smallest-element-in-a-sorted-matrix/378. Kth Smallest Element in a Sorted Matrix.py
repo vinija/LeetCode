f3e325e732b2f6b266f552ceee14aa454332df60
Vinija Jain <vinija@gmail.com>
2024-09-14 18:36:49 -0700

    Runtime: 309 ms (5.10%), Memory: 18.7 MB (100.00%) - Gitcode

--- 378-kth-smallest-element-in-a-sorted-matrix/378. Kth Smallest Element in a Sorted Matrix.py
+++ 378-kth-smallest-element-in-a-sorted-matrix/378. Kth Smallest Element in a Sorted Matrix.py
@@ -5,24 +5,35 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
-        def findCount(matrix, val):
-            count = 0
-            for row in matrix:
-                for col in row:
-                    if col <= val: count += 1
-            return count
+        def count_less_equal(matrix, smaller, mid, larger):
+            count, n = 0, len(matrix)
+            row, col = n - 1, 0
+            while row >= 0 and col < n:
+                if matrix[row][col] > mid:
+                  # as matrix[row][col] is bigger than the mid, let's keep track of the
+                  # smallest number greater than the mid
+                  larger = min(larger, matrix[row][col])
+                  row -= 1
+                else:
+                  # as matrix[row][col] is less than or equal to the mid, let's keep track of the
+                  # biggest number less than or equal to the mid
+                  smaller = max(smaller, matrix[row][col])
+                  count += row + 1
+                  col += 1
+
+            return count, smaller, larger
             
         l, r = matrix[0][0], matrix[-1][-1]
         while l < r:
             mid = (l + r) // 2 # or l + (r - l) // 2
             
             # calculate how many numbers are on the left of middle number
-            count = findCount(matrix, mid)
+            count, smaller, larger = count_less_equal(matrix, l, mid, r)
             # or order = sum(bisect.bisect(row, m) for row in matrix) < k:  
             
             if count >= k:
-                r = mid # search lower
+                r = smaller # search lower
             else:
-                l = mid + 1 # search higher
+                l = larger # search higher
                 
         return l

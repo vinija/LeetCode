0a897e69b577c8d740483eaf43be1366d0dadd49
Vinija Jain <vinija@gmail.com>
2024-09-14 17:54:32 -0700

    Runtime: 70 ms (5.09%), Memory: 14.5 MB (100.00%) - Gitcode

--- 139-word-break/139. Word Break.py
+++ 139-word-break/139. Word Break.py
@@ -1,11 +1,23 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        dp = [0]*(len(s)+1)
-        dp[0] = 1
-        # we mark as true every index that we managed to segment so far
-        for i in range(1, len(s)+1):
-            for j in range(i):
-                if dp[j] and s[j:i] in wordDict:
-                    dp[i] = 1
-                    break
+        dp = [True]+[False] * len(s)
+        
+        for index in range(1,len(s)+1):
+            
+            for word in wordDict:
+                if dp[index - len(word)] and s[:index].endswith(word):
+                    dp[index] = True
+                    
         return dp[-1]
+
+# class Solution:
+#     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+#         dp = [True] + [False] * len(s)
+        
+#         for indx in range(1, len(s) + 1):
+            
+#             for word in wordDict:
+#                 if dp[indx - len(word)] and s[:indx].endswith(word):
+#                     dp[indx] = True
+            
+#         return dp[-1]
